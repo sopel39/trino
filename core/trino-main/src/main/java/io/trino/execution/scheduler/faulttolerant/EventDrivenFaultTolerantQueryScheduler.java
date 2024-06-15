a4411ee74652189b43100a1c06976e1170ea16ab
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-06-14 23:49:26 -0400

    Add cache split scheduling
    Goal: Try not to schedule splits with same
    cache id at the same time.
    
    This commit has changes at two level:
    
    First, we re-arrange splits coming out of
    CacheSplitSource such that splits with same
    CacheSplitId has some separation and are not
    processed in parallel.
    
    Second at worker level, we try not to schedule
    splits with same CacheSplitId. However, to make
    progress, splits with same CacheSplitId will be
    scheduled if there are no splits with unique
    CachesplitId  This is needed to avoid potential deadlock.
    
    Benchmarks:
    Query:
    select sum(orderkey / 10000), sum(partkey / 10000),
    sum(suppkey / 10000), sum(linenumber), sum(quantity)
     from lineitem
    union all
    select sum(orderkey / 10000), sum(partkey / 10000),
    sum(suppkey / 10000), sum(linenumber), sum(quantity)
    from lineitem;
    
    Query 20240614_234254_00006_ehfpk, FINISHED, 9 nodes
    Splits: 9,909 total, 9,909 done (100.00%)
    9.68 [6.21B rows, 107GB] [641M rows/s, 11.1GB/s]
    
    As you can see we only read ~6B rows as compare
    to 12B rows which is the actual data.

--- core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -42,6 +42,7 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.trino.Session;
+import io.trino.cache.SplitAdmissionControllerProvider;
 import io.trino.cost.RuntimeInfoProvider;
 import io.trino.cost.StaticRuntimeInfoProvider;
 import io.trino.exchange.ExchangeContextInstance;
@@ -740,6 +741,8 @@ public class EventDrivenFaultTolerantQueryScheduler
 
         private boolean queryOutputSet;
 
+        private final SplitAdmissionControllerProvider splitAdmissionControllerProvider;
+
         public Scheduler(
                 QueryStateMachine queryStateMachine,
                 Metadata metadata,
@@ -820,6 +823,9 @@ public class EventDrivenFaultTolerantQueryScheduler
             }
 
             planInTopologicalOrder = sortPlanInTopologicalOrder(plan);
+            splitAdmissionControllerProvider = new SplitAdmissionControllerProvider(
+                    planInTopologicalOrder.stream().map(SubPlan::getFragment).collect(toImmutableList()),
+                    queryStateMachine.getSession());
             noEventsStopwatch.start();
         }
 
@@ -1426,7 +1432,8 @@ public class EventDrivenFaultTolerantQueryScheduler
                         queryStateMachine.getStateMachineExecutor(),
                         tracer,
                         schedulerSpan,
-                        schedulerStats);
+                        schedulerStats,
+                        splitAdmissionControllerProvider);
                 closer.register(stage::abort);
                 stageRegistry.add(stage);
                 stage.addFinalStageInfoListener(status -> queryStateMachine.updateQueryInfo(Optional.ofNullable(stageRegistry.getStageInfo())));
@@ -1464,7 +1471,8 @@ public class EventDrivenFaultTolerantQueryScheduler
                         sourceExchanges,
                         partitioningSchemeFactory.get(fragment.getPartitioning(), fragment.getPartitionCount()),
                         stage::recordGetSplitTime,
-                        outputDataSizeEstimates.buildOrThrow()));
+                        outputDataSizeEstimates.buildOrThrow(),
+                        splitAdmissionControllerProvider));
 
                 FaultTolerantPartitioningScheme sinkPartitioningScheme = partitioningSchemeFactory.get(
                         fragment.getOutputPartitioningScheme().getPartitioning().getHandle(),
